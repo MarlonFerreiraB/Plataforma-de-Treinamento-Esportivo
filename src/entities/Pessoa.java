b4ab05ca52f572f09da5b3f244e8f02a500ca8a1
Marlon Ferreira <mferreirab03@gmail.com>
2025-05-01 19:03:15 -0300

    feat: estrutura criada.

--- src/entities/Pessoa.java
+++ src/entities/Pessoa.java
@@ -1,4 +1,39 @@
 package entities;
 
-public class Pessoa {
+import enums.PerfilUsuario;
+import exception.DataNascimentoException;
+
+import javax.sound.sampled.Port;
+import java.time.LocalDate;
+
+public abstract class Pessoa {
+    protected String name;
+    protected String email;
+    protected LocalDate dataNascimento;
+    protected PerfilUsuario perfil;
+
+    public Pessoa(String name,String email, LocalDate dataNascimento, PerfilUsuario perfil) throws DataNascimentoException {
+        this.name =name;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.perfil = perfil;
+        if(dataNascimento.isAfter(LocalDate.now())){
+            throw new DataNascimentoException("Error: Nascimento esta errado");
+        }
+
+    }
+
+    public String getName(){return name;}
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public PerfilUsuario getPerfil() {
+        return perfil;
+    }
 }
